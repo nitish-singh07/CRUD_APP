6ca3d4c01f4bd4fe8cad46f23935bbc86c653987
Nitish Singh <152492057+nitish-singh07@users.noreply.github.com>
2024-02-25 12:26:34 +0530

    Add files via upload
    all file uploaded

---
+++ EmsSystem/src/test/java/com/example/EmsSystem/EmsSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EmsSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmsSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
